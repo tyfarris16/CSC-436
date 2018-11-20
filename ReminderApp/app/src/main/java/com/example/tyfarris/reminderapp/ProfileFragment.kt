57869d9081346281cd95b0506c260a0cc91e3218
Ty Farris <tefarris@calpoly.edu>
2018-11-19 19:28:24 -0800

    Working Text to Speech

--- ReminderApp/app/src/main/java/com/example/tyfarris/reminderapp/ProfileFragment.kt
+++ ReminderApp/app/src/main/java/com/example/tyfarris/reminderapp/ProfileFragment.kt
@@ -1,14 +1,35 @@
 package com.example.tyfarris.reminderapp
 
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ProgressBar
+import java.util.*
 
 class ProfileFragment : Fragment(){
 
+    var tts:TextToSpeech? = null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+        val progressLevel = view.findViewById<ProgressBar>(R.id.progressBar)
+        val quoteOfDayBtn = view.findViewById<Button>(R.id.quoteButton)
+
+        progressLevel.progress = 50
+
+        tts = TextToSpeech(activity?.applicationContext, TextToSpeech.OnInitListener { status ->
+            if (status != TextToSpeech.ERROR) {
+                tts!!.setLanguage(Locale.UK)
+            }
+        })
+
+        quoteOfDayBtn!!.setOnClickListener{
+            tts!!.speak("Just Keep Swimming", TextToSpeech.QUEUE_FLUSH, null, null)
+        }
+        return view
     }
 }
